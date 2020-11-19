7103d6ce9bc4630458fe8e06ccd3ae432090db77
Ehsan Taghavi <ehsantgv@gmail.com>
2020-11-19 23:22:41 +0330

    Showing badge

--- app/src/main/java/com/taghavi/bottomnavigationbarbadge/MainActivity.kt
+++ app/src/main/java/com/taghavi/bottomnavigationbarbadge/MainActivity.kt
@@ -2,10 +2,31 @@ package com.taghavi.bottomnavigationbarbadge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addBadge(1)
+    }
+
+    private fun addBadge(count: Int) {
+        val badge: BadgeDrawable = nav_view.getOrCreateBadge(
+            R.id.navigation_notifications
+        )
+        badge.number = count
+        badge.isVisible = true
+    }
+
+    private fun removeBadge() {
+        nav_view?.removeBadge(R.id.navigation_notifications)
     }
 }
